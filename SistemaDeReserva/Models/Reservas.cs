b5b260a6d8bd53a527603ffa2652584954caaa7b
Matheus Araujo <matheus2001sousa@hotmail.com>
2024-10-07 08:50:34 -0300

    commit inicial

---
+++ SistemaDeReserva/Models/Reservas.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SistemaDeReserva.Models
+{
+    public class Reservas
+    {
+        private decimal precoPorDiaria = 0;
+        private List<string> reserva = new List<string>();
+
+        public Reservas(decimal precoPorDiaria)
+        {
+            this.precoPorDiaria=precoPorDiaria;
+        }
+        public void AdicionarReserva()
+        {
+            Console.WriteLine("Digite o nome do cliente:");
+            string cliente = Console.ReadLine();
+            
+            reserva.Add(cliente);
+            Console.WriteLine($"{cliente} foi hospedado com sucesso!");
+        }
+        public void RemoverReserva()
+        {
+            Console.WriteLine("Digite o nome do cliente para remover:");
+            string cliente = Console.ReadLine();
+
+            if(reserva.Any())
+            {
+                Console.WriteLine("Digite quantas diárias o cliente permaneceu no hotel:");
+                int dias = int.Parse(Console.ReadLine());
+
+                decimal valorTotal = precoPorDiaria * dias;
+
+                reserva.Remove(cliente);
+
+                Console.WriteLine($"O cliente {cliente} foi removido com sucesso e o preço total foi de: R$ {valorTotal}");
+            }
+            else
+                Console.WriteLine("Desculpa, esse cliente não está hospedado aqui. Confira se digitou corretamente");
+        }
+        public void ListarReserva()
+        {
+            if(reserva.Any())
+            {
+                Console.WriteLine("Os clientes hospedados são:");
+                foreach (var cliente in reserva)
+                {
+                    Console.WriteLine(cliente);
+                }
+            }
+            else
+                Console.WriteLine("Não há clientes hospedados.");
+        }
+        
+
+    }
+}
---
+++ SistemaDeReserva/Program.cs
@@ -0,0 +1,43 @@
+﻿using SistemaDeReserva.Models;
+
+decimal precoPorDiaria = 0;
+
+Console.WriteLine("Seja bem vindo ao sistema de hospedagem!\n" + "Digite o valor da diária:");
+precoPorDiaria = Convert.ToDecimal(Console.ReadLine());
+
+Reservas rsv = new Reservas(precoPorDiaria);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar hospede");
+    Console.WriteLine("2 - Remover hospede");
+    Console.WriteLine("3 - Listar hospedagens");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            rsv.AdicionarReserva();
+            break;
+        case "2":
+            rsv.RemoverReserva();
+            break;
+        case "3":
+            rsv.ListarReserva();
+            break;
+        case "4":
+            exibirMenu = false;
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+Console.WriteLine("O programa se encerrou");
